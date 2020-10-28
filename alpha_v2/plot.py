5370919e5c9b0cd0978d5a43b9962f4bfb794b94
Pax <olydaun@hotmail.fr>
2020-10-28 11:49:01 +0100

    report

--- alpha_v2/plot.py
+++ alpha_v2/plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from parameters import Nx, Ny
+from parameters import Nx, Ny, geometry
 import matrices as m
 
 
@@ -51,3 +51,73 @@ class Plot:
 
         fig.colorbar(im, ax=ax4)
         plt.savefig('temp.pdf')
+
+    def plot_graphs(self, display):
+
+        matrices = m.Matrices()
+        fig, ax = plt.subplots()
+
+        ax.imshow(matrices.G, cmap='coolwarm', interpolation='none')
+        ax.axis('off')
+
+        if display == "potential":
+            self.plot_potential(ax, "potential")
+
+        if display == "velocity":
+            self.plot_velocity(ax, "velocity")
+
+        if display == "streamlines":
+            self.plot_streamlines(ax, "streamlines")
+
+        if display == "pressure":
+            self.plot_pressure(ax, "pressure")
+
+        plt.savefig("figures/{}_{}_x={}_y={}.pdf"\
+            .format(display, geometry, Nx, Ny))
+
+    def plot_potential(self, ax, display):
+
+        matrices = m.Matrices()
+
+        ax.set_title('Velocity potential field', fontsize=10)
+        ax.contour(matrices.phi, levels=Nx,
+                   colors='green', linewidths=.75)
+
+        return ax
+
+    def plot_velocity(self, ax, display):
+
+        matrices = m.Matrices()
+
+        ax.set_title('Velocity field', fontsize=10)
+
+        X = np.linspace(0, Nx - 1, Nx)
+        Y = np.linspace(0, Ny - 1, Ny)
+        XX, YY = np.meshgrid(X, Y)
+
+        ax.quiver(XX - .25, YY, -matrices.grad_own[2], matrices.grad_own[3])
+
+        return ax
+
+    def plot_streamlines(self, ax, display):
+
+        matrices = m.Matrices()
+
+        ax.set_title('Streamlines', fontsize=10)
+        ax.streamplot(np.linspace(0, Nx - 1, Nx), np.linspace(0, Ny - 1, Ny),
+                      -matrices.grad_own[0], -matrices.grad_own[1],
+                      linewidth=.75, arrowsize=.75)
+
+        return ax
+
+    def plot_pressure(self, ax, display):
+
+        matrices = m.Matrices()
+
+        ax.set_title('Pressure field and isobaric lines', fontsize=10)
+
+        ax.contour(matrices.pressure, levels=10,
+                   colors='olivedrab', linewidths=.75)
+        im = ax.imshow(matrices.pressure, cmap='jet')
+
+        return ax
